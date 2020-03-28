6d1b710ca3470083f3311742a44b9f7735dadd90
grad6352 <56140837+grad6352@users.noreply.github.com>
2020-03-27 17:10:37 -0700

    updated python programs
    Updated gpio_interrupt_2.py and TEAMTBD_sensor01.py
    Switched over to TEAMTBD_sensor01.py, other python program unused
    removed test_only_newlines.py

--- TEAMTBD_sensor01.py
+++ TEAMTBD_sensor01.py
@@ -1,25 +1,36 @@
 import RPi.GPIO as GPIO
 import datetime
+from datetime import datetime
+from threading import Timer
+import os
 
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-print("Waiting for MAHIVE event on port 23")
+#print("Waiting for MAHIVE event on port 23")
 
 try:
-	f1 = open('textlog.txt', 'w+')
-	f1.close()
+	xTime = datetime.today()
+	
+	f1 = open('sensor01_buffer.txt', 'w').close()
+	f = open('sensor01_archive.txt', 'w').close()
 	while True:
 		GPIO.wait_for_edge(23, GPIO.FALLING)
-		print("\nMAHIVE event detected")
-		time = datetime.datetime.now()
+		time = datetime.now()
 		timeStr = time.strftime("%m/%d/%Y, %H:%M:%S")
-		with open('textlog.txt', 'a') as file1:
-			file1.write("\nTimestamp: ")
+		f = open('sensor01_buffer.txt', 'w').close()
+		with open('sensor01_buffer.txt', 'a') as file1:
+			file1.write("Timestamp: ")
 			file1.write(timeStr)	
+		with open('sensor01_archive.txt', 'a') as file2:
+			file2.write("Timestamp: ")
+			file2.write(timeStr)
+			file2.write('\n')
 except KeyboardInterrupt:
 	GPIO.cleanup()
 GPIO.cleanup()
-#file1.close()
-	
+
+def renameArchive():
+	dateStr = datetime.
+	os.rename('sensor01_archive.txt', 
--- gpio_interrupt_2.py
+++ gpio_interrupt_2.py
@@ -5,21 +5,26 @@ GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-print("Waiting for MAHIVE event on port 23")
+#print("Waiting for MAHIVE event on port 23")
 
 try:
-	f1 = open('textlog.txt', 'w+')
-	f1.close()
+	f1 = open('button_demo_buffer.txt', 'w').close()
+	f = open('button_demo_archive.txt', 'w').close()
 	while True:
 		GPIO.wait_for_edge(23, GPIO.FALLING)
-		print("\nMAHIVE event detected")
+		#print("\nMAHIVE event detected")
 		time = datetime.datetime.now()
 		timeStr = time.strftime("%m/%d/%Y, %H:%M:%S")
-		with open('textlog.txt', 'a') as file1:
-			file1.write("\nTimestamp: ")
+		f = open('button_demo_buffer.txt', 'w').close()
+		with open('button_demo_buffer.txt', 'a') as file1:
+			file1.write("Timestamp: ")
 			file1.write(timeStr)	
+		with open('button_demo_archive.txt', 'a') as file2:
+			file2.write("Timestamp: ")
+			file2.write(timeStr)
+			file2.write('\n')
 except KeyboardInterrupt:
 	GPIO.cleanup()
 GPIO.cleanup()
-#file1.close()
+
 	
