3f7ea6f0ec529f4d5aa6942e5e1a98e4a110ad66
grad6352 <56140837+grad6352@users.noreply.github.com>
2020-03-30 16:06:05 -0700

    Second sensor
    Adding in code to handle second sensor

--- TEAMTBD_sensor01.py
+++ TEAMTBD_sensor01.py
@@ -6,12 +6,13 @@ from threading import Timer
 import os
 
 GPIO.setmode(GPIO.BCM)
-
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(25, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-def renameArchive():
-	newNameStr = datetime.now().strftime("%Y%m%d%H%M%S") + "_archive_sensor01.txt"
-	os.rename('sensor01_archive.txt', newNameStr)
+f1_0 = open('sensor01_buffer.txt', 'w').close()
+f1_1 = open('sensor01_archive.txt', 'w').close()
+f2_0 = open('sensor02_buffer.txt', 'w').close()
+f2_1 = open('sensor02_archive.txt', 'w').close()
 
 try:
 	xTime = datetime.today()
@@ -20,23 +21,37 @@ try:
 
 	secs=delta_t.seconds+1
 
-	t = Timer(secs, renameArchive())
+	t = Timer(secs, renameArchives())
 	t.start
 
-	f1 = open('sensor01_buffer.txt', 'w').close()
-	f = open('sensor01_archive.txt', 'w').close()
 	while True:
 		GPIO.wait_for_edge(23, GPIO.FALLING)
+		GPIO.wait_for_edge(25, GPIO.FALLING)
 		time = datetime.now()
 		timeStr = time.strftime("%m/%d/%Y, %H:%M:%S")
-		f = open('sensor01_buffer.txt', 'w').close()
-		with open('sensor01_buffer.txt', 'a') as file1:
-			file1.write("Timestamp: ")
-			file1.write(timeStr)	
-		with open('sensor01_archive.txt', 'a') as file2:
-			file2.write("Timestamp: ")
-			file2.write(timeStr)
-			file2.write('\n')
+		f0 = open('sensor01_buffer.txt', 'w').close()
+		f0 = open('sensor02_buffer.txt', 'w').close()
+		with open('sensor01_buffer.txt', 'a') as file1_0:
+			file1_0.write("Timestamp: ")
+			file1_0.write(timeStr)	
+		with open('sensor01_archive.txt', 'a') as file1_1:
+			file1_1.write("Timestamp: ")
+			file1_1.write(timeStr)
+			file1_1.write('\n')
+		with open('sensor02_buffer.txt', 'a') as file2_0:
+			file2_0.write("Timestamp: ")
+			file2_0.write(timeStr)
+		with open('sensor02_archive.txt', 'a') as file2_1:
+			file2_1.write("Timestamp: ")
+			file2_1.write(timeStr)
+			file2_1.write('\n')
 except KeyboardInterrupt:
 	GPIO.cleanup()
 GPIO.cleanup()
+
+def renameArchives():
+	datetimeStr = datetime.now.strftime("%Y%m%d%H%M%S")
+	newSensor1Str = datetimeStr + "_archive_sensor01.txt"
+	os.rename('sensor01_archive.txt', newSensor1Str)
+	newSensor2Str = datetimeStr + "_archive_sensor02.txt"
+	os.rename('sensor02_archive.txt', newSensor2Str)
