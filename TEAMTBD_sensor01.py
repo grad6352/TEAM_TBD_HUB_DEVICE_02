aeeaa5e8df128d2e37f05a0e11e6c7e3db8fbbe8
grad6352 <56140837+grad6352@users.noreply.github.com>
2020-03-30 16:54:35 -0700

    2 Sensors now work
    refactor code, separate into separate functions for modularity
    
    Uses GPIO pins 23 and 25 as inputs for sensors

--- TEAMTBD_sensor01.py
+++ TEAMTBD_sensor01.py
@@ -1,36 +1,68 @@
 import RPi.GPIO as GPIO
-import datetime
+#import datetime
 from datetime import datetime
 from datetime import date
 from threading import Timer
 import os
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(25, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+pin_list = [23, 25]
+sensor_list = ["sensor01", "sensor02"]
 
-f1_0 = open('sensor01_buffer.txt', 'w').close()
-f1_1 = open('sensor01_archive.txt', 'w').close()
-f2_0 = open('sensor02_buffer.txt', 'w').close()
-f2_1 = open('sensor02_archive.txt', 'w').close()
+def gpio_setup(list):
+	GPIO.setmode(GPIO.BCM)
+	for number in list:
+		GPIO.setup(number, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-try:
+def renameArchive(sensorList):
+	datetimeNow = datetime.now()
+	timestampStr = datetimeNow.strftime("%Y%b%d%H%M%S")
+
+	for sensor in sensorList:
+		archiveStr = sensor + "_archive.txt"
+		newArchiveStr = timestampStr + "_archive_" + sensor + ".txt"
+		os.rename(archiveStr, newArchiveStr)
+
+def timer_setup():
 	xTime = datetime.today()
 	yTime = xTime.replace(day=xTime.day+1, hour=1, minute=0, second=0, microsecond=0)
 	delta_t=yTime-xTime
-
+	
 	secs=delta_t.seconds+1
-
-	t = Timer(secs, renameArchives())
+	
+	t = Timer(secs, renameArchive(sensor_list))
 	t.start
 
+def sensor_setup(sensorList):
+	for sensor in sensorList:
+		bufferStr = sensor + "_buffer.txt"
+		archiveStr = sensor + "_archive.txt"
+		f0 = open(bufferStr, 'w').close()
+		f1 = open(archiveStr, 'w').close()
+		
+def sensor02_callback(channel):
+	time = datetime.now()
+	timeStr = time.strftime("%m/%d/%Y, %H:%M:%S")
+	f = open('sensor02_buffer.txt', 'w').close()
+	with open('sensor02_buffer.txt', 'a') as file2_0:
+		file2_0.write("Timestamp: ")
+		file2_0.write(timeStr)
+	with open('sensor02_archive.txt', 'a') as file2_1:
+		file2_1.write("Timestamp: ")
+		file2_1.write(timeStr)
+		file2_1.write('\n')
+
+gpio_setup(list=pin_list)
+sensor_setup(sensorList=sensor_list)
+timer_setup()
+
+GPIO.add_event_detect(25, GPIO.FALLING, callback=sensor02_callback, bouncetime=300)
+		
+try:
 	while True:
 		GPIO.wait_for_edge(23, GPIO.FALLING)
-		GPIO.wait_for_edge(25, GPIO.FALLING)
 		time = datetime.now()
 		timeStr = time.strftime("%m/%d/%Y, %H:%M:%S")
-		f0 = open('sensor01_buffer.txt', 'w').close()
-		f0 = open('sensor02_buffer.txt', 'w').close()
+		f = open('sensor01_buffer.txt', 'w').close()
 		with open('sensor01_buffer.txt', 'a') as file1_0:
 			file1_0.write("Timestamp: ")
 			file1_0.write(timeStr)	
@@ -38,20 +70,6 @@ try:
 			file1_1.write("Timestamp: ")
 			file1_1.write(timeStr)
 			file1_1.write('\n')
-		with open('sensor02_buffer.txt', 'a') as file2_0:
-			file2_0.write("Timestamp: ")
-			file2_0.write(timeStr)
-		with open('sensor02_archive.txt', 'a') as file2_1:
-			file2_1.write("Timestamp: ")
-			file2_1.write(timeStr)
-			file2_1.write('\n')
 except KeyboardInterrupt:
 	GPIO.cleanup()
 GPIO.cleanup()
-
-def renameArchives():
-	datetimeStr = datetime.now.strftime("%Y%m%d%H%M%S")
-	newSensor1Str = datetimeStr + "_archive_sensor01.txt"
-	os.rename('sensor01_archive.txt', newSensor1Str)
-	newSensor2Str = datetimeStr + "_archive_sensor02.txt"
-	os.rename('sensor02_archive.txt', newSensor2Str)
