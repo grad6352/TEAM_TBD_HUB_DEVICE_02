908c3a27e725ff937794c7f569ac57cfdd947844
grad6352 <56140837+grad6352@users.noreply.github.com>
2020-04-18 11:53:31 -0700

    Update TEAMTBD_function01.py
    Tried implementing for loop to open and retrieve data from file

--- TEAMTBD_function01.py
+++ TEAMTBD_function01.py
@@ -51,14 +51,19 @@ def greengrass_hello_world_run():
         #        payload="Hello world! Sent from " "Greengrass Core running on platform: {}".format(my_platform),
         #    )
         sensors = ["sensor01", "sensor02"]
-        with open('/home/pi/sensor02_buffer.txt', 'r') as log:
-            data1 = log.read()
-        if data1 != greengrass_hello_world_run.previous_value:
-            #data = "asdfjkl;"
-            #client.publish(topic="teamtbd/hub", queueFullPolicy="AllOrException", payload=data)
-        #if data != greengrass_hello_world_run.previous_value:
-        #else:
-            client.publish(topic="teamtbd/hub", queueFullPolicy="AllOrException", payload=data1)
+        for sensor in sensors:
+            buffer = '/home/pi/' + sensor + '_buffer.txt'
+            
+            with open('/home/pi/sensor02_buffer.txt', 'r') as log:
+                data1 = log.read()
+            if data1 != greengrass_hello_world_run.previous_value_1:
+                if data1 != greengrass_hello_world_run.previous_value_2:
+                    if data1 != greengrass_hello_world_run.initial_value:
+                        client.publish(topic="teamtbd/hub", queueFullPolicy="AllOrException", payload=data1)
+                        if sensor == sensors[0]:
+                            greengrass_hello_world_run.previous_value_1 = data1
+                        else if sensor == sensors[1]
+                            greengrass_hello_world_run.previous_value_2 = data1
         #    greengrass_hello_world_run.previous_value = data
     except Exception as e:
         logger.error("Failed to publish message: " + repr(e))
@@ -68,7 +73,9 @@ def greengrass_hello_world_run():
 
 
 # Start executing the function above
-greengrass_hello_world_run.previous_value = ''
+greengrass_hello_world_run.previous_value_1 = ''
+greengrass_hello_world_run.previous_value_2 = ''
+greengrass_hello_world_run.initial_value = ''
 
 greengrass_hello_world_run()
 
