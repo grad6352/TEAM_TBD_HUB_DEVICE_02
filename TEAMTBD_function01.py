94266e9463fb5917b3e0a4cfe3c7e5ebc6f1e9d3
grad6352 <56140837+grad6352@users.noreply.github.com>
2020-04-18 12:06:12 -0700

    Update TEAMTBD_function01.py
    Removed unnecessary lines
    The copyright notice was not relevant anymore, as the function code is much different now than before and doesn't use Amazon's example code

--- TEAMTBD_function01.py
+++ TEAMTBD_function01.py
@@ -1,16 +1,3 @@
-#
-# Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-#
-
-# greengrassHelloWorld.py
-# Demonstrates a simple publish to a topic using Greengrass core sdk
-# This lambda function will retrieve underlying platform information and send
-# a hello world message along with the platform information to the topic
-# 'hello/world'. The function will sleep for five seconds, then repeat.
-# Since the function is long-lived it will run forever when deployed to a
-# Greengrass core.  The handler will NOT be invoked in our example since
-# the we are executing an infinite loop.
-
 import logging
 import platform
 import sys
@@ -30,12 +17,6 @@ client = greengrasssdk.client("iot-data")
 my_platform = platform.platform()
 
 
-# When deployed to a Greengrass core, this code will be executed immediately
-# as a long-lived lambda function.  The code will enter the infinite while
-# loop below.
-# If you execute a 'test' on the Lambda Console, this test will fail by
-# hitting the execution timeout of three seconds.  This is expected as
-# this function never returns a result.
 def openSensorFile(filename):
     with open(filename, 'r') as log:
         data = log.read()
@@ -43,16 +24,6 @@ def openSensorFile(filename):
 
 def greengrass_hello_world_run():
     try:
-        #if not my_platform:
-        #    client.publish(
-        #        topic="hello/world", queueFullPolicy="AllOrException", payload="Hello world! Sent from Greengrass Core."
-        #    )
-        #else:
-        #    client.publish(
-        #        topic="hello/world",
-        #        queueFullPolicy="AllOrException",
-        #        payload="Hello world! Sent from " "Greengrass Core running on platform: {}".format(my_platform),
-        #    )
         sensorFiles = ["/home/pi/sensor01_buffer.txt", "/home/pi/sensor02_buffer.txt"]
         
         data1 = openSensorFile(sensorFiles[0])
@@ -68,25 +39,11 @@ def greengrass_hello_world_run():
         greengrass_hello_world_run.previous_value_1 = data1
         greengrass_hello_world_run.previous_value_2 = data2
         
-        #for sensor in sensorFiles:
-        #    with open(sensor, 'r') as log:
-        #        data1 = log.read()
-        #    if data1 != greengrass_hello_world_run.previous_value_1:
-        #        if data1 != greengrass_hello_world_run.previous_value_2:
-        #            if data1 != greengrass_hello_world_run.initial_value:
-        #                client.publish(topic="teamtbd/hub", queueFullPolicy="AllOrException", payload=data1)
-        #                if sensor == sensors[0]:
-        #                    greengrass_hello_world_run.previous_value_1 = data1
-        #                else if sensor == sensors[1]
-        #                    greengrass_hello_world_run.previous_value_2 = data1
-        #            else
-        #                client.publish(topic="teamtbd/hub", queueFullPolicy="AllOrException", payload="foo")
-        #    greengrass_hello_world_run.previous_value = data
     except Exception as e:
         logger.error("Failed to publish message: " + repr(e))
 
     # Asynchronously schedule this function to be run again in 5 seconds
-    Timer(5, greengrass_hello_world_run).start()
+    Timer(1, greengrass_hello_world_run).start()
 
 
 # Start executing the function above
